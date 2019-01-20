eac3ce126c588cf67b4229916c3e31e38289859f
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-01-20 14:50:36 -0300

    Tweet model created

--- MiniTwitter/tweet/models.py
+++ MiniTwitter/tweet/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+
+class Tweet(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='tweets'
+    )
+    content = models.TextField(
+        max_length=280
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now
+    )
+    likes = models.BigIntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        return self.content
+
